420498f9b58d6b7151869a0231ede6ccd830e7ac
SERVERS\paw.byrialsen <paw.byrialsen@stevanatogroup.com>
2018-11-20 15:50:18 +0100

    Add project files.

---
+++ TestCatelPage/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestCatelPage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Catel.Windows.Controls.Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ TestCatelPage/ModuleInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCatelPage
+{
+    public static class ModuleInitializer
+    {
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        public static void Initialize()
+        {
+        }
+    }
+}
